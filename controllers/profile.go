de543cb669ba0fabdd3e14d59094dfc3973df927
Tripamungkas Kartika Adi <tripamungkas.adi@gmail.com>
2021-05-17 17:05:40 +0700

    Implemented Some Features
    1. Implemented Delete profile function
    2. Add supporting routes for hobby

--- controllers/profile.go
+++ controllers/profile.go
@@ -19,6 +19,11 @@ type ProfileForm struct {
 	Age  int    `form:"age,text" valid:"required"`
 }
 
+type HobbyForm struct {
+	Id    int    `form:"-"`
+	Hobby string `form:"hobby,text" valid:"required"`
+}
+
 type ProfileController struct {
 	beego.Controller
 }
@@ -38,11 +43,13 @@ func (c *ProfileController) ListProfile() {
 	c.LayoutSections["Footer"] = "base/footer.html"
 	c.LayoutSections["Header"] = "base/header.html"
 	c.Data["xsrf_token"] = c.XSRFToken()
-	c.Data["xsrfdata"] = template.HTML(c.XSRFFormHTML())
+	c.Data["Xsrfdata"] = template.HTML(c.XSRFFormHTML())
 	c.Data["Results"] = profiles
 
+	fmt.Println(profiles)
+
 	if c.GetSession("success") == true {
-		c.Data["SuccessMessage"] = "Data has been successfully submitted!"
+		c.Data["SuccessMessage"] = "Data has been successfully submitted/deleted!"
 		c.SetSession("success", false)
 	}
 }
@@ -61,10 +68,8 @@ func (c *ProfileController) AddProfile() {
 	if err := c.ParseForm(&form); err != nil {
 		log.Println("[Err] Error on AddProfile : ", err.Error())
 		c.Abort("500")
-		return
 	}
-	fmt.Println(form)
-	c.Data["Form"] = &ProfileForm{}
+	c.Data["Form"] = form
 
 	// Fetch all user profiles
 	var profiles []models.Profile
@@ -104,5 +109,88 @@ func (c *ProfileController) AddProfile() {
 }
 
 func (c *ProfileController) AddHobby() {
+	idProfile := c.Ctx.Input.Param(":idProfile")
+	profile := models.Profile{}
+
+	err := database.Conn.First(&profile, idProfile).Error
+	if err != nil {
+		log.Println("[Err] DB error on fetching profile with id = ", idProfile)
+	}
+
+	// Render Form
+	c.Data["Form"] = &HobbyForm{}
+
+	// Render Template
+	c.Layout = "base/base.html"
+	c.TplName = "hobby.html"
+	c.LayoutSections = make(map[string]string)
+	c.LayoutSections["Footer"] = "base/footer.html"
+	c.LayoutSections["Header"] = "base/header.html"
+	c.Data["xsrf_token"] = c.XSRFToken()
+	c.Data["Xsrfdata"] = template.HTML(c.XSRFFormHTML())
+	c.Data["Profile"] = profile
+}
+
+func (c *ProfileController) SubmitHobby() {
+	idProfile := c.Ctx.Input.Param(":idProfile")
+	profile := models.Profile{}
+
+	err := database.Conn.First(&profile, idProfile).Error
+	if err != nil {
+		log.Println("[Err] DB error on fetching profile with id = ", idProfile)
+	}
+
+	// Templates
+	c.Layout = "base/base.html"
+	c.TplName = "hobby.html"
+	c.LayoutSections = make(map[string]string)
+	c.LayoutSections["Footer"] = "base/footer.html"
+	c.LayoutSections["Header"] = "base/header.html"
+	c.Data["xsrf_token"] = c.XSRFToken()
+	c.Data["xsrfdata"] = template.HTML(c.XSRFFormHTML())
+
+	form := HobbyForm{}
+	if err := c.ParseForm(&form); err != nil {
+		log.Println("[Err] Error on SubmitHobby : ", err.Error())
+		c.Abort("500")
+	}
+	c.Data["Form"] = form
+
+	valid := validation.Validation{}
+	valid.Required(form.Hobby, "Hobby").Message("is required")
+
+	errMessages := make(map[string]string)
+	if valid.HasErrors() {
+		for _, err := range valid.Errors {
+			log.Println("[FormEror] ", err.Key, " : ", err.Message)
+			errMessages[err.Key] = err.Message
+		}
+		c.Data["ErrMessages"] = errMessages
+		return
+	}
+
+	newHobby := models.Hobby{
+		Hobby:     form.Hobby,
+		ProfileID: profile.ID,
+	}
+	database.Conn.Create(&newHobby)
+
+	c.SetSession("success", true)
+	c.Redirect("/profile", 302)
+}
+
+func (c *ProfileController) DeleteProfile() {
+	id := c.Ctx.Input.Param(":idProfile")
+	err := database.Conn.Delete(&models.Profile{}, id).Error
+	if err != nil {
+		log.Println("[Err] Error on deleting profile with id = ", id)
+		c.Abort("500")
+	}
+
+	c.SetSession("success", false)
+	c.Redirect("/profile", 302)
+}
+
+func (c *ProfileController) DeleteHobby() {
 	return
 }
--- routers/router.go
+++ routers/router.go
@@ -11,7 +11,10 @@ func init() {
 	profile := beego.NewNamespace("/profile",
 		beego.NSRouter("", &controllers.ProfileController{}, "get:ListProfile"),
 		beego.NSRouter("/add", &controllers.ProfileController{}, "post:AddProfile"),
-		beego.NSRouter("/:id/addHobby", &controllers.ProfileController{}, "post:AddHobby"),
+		beego.NSRouter("/:idProfile/addHobby", &controllers.ProfileController{}, "get:AddHobby"),
+		beego.NSRouter("/:idProfile/addHobby/submit", &controllers.ProfileController{}, "post:SubmitHobby"),
+		beego.NSRouter("/:idProfile/deleteProfile", &controllers.ProfileController{}, "post:DeleteProfile"),
+		beego.NSRouter("/:idProfile/hobby/:idHobby/delete", &controllers.ProfileController{}, "post:DeleteHobby"),
 	)
 	beego.AddNamespace(profile)
 }
